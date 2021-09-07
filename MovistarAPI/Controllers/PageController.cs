3955206c98354e3e9cb82edef4c835814bf2a912
Laura Camila Joya Aroca <ljoya@mareigua.co>
2021-09-06 22:11:14 -0500

    Se crea página web asociada a Hoja de vida

---
+++ MovistarAPI/Controllers/PageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovistarAPI.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View("Educacion");
+        }
+        
+        public IActionResult LaboralExperience()
+        {
+            return View("ExperienciaLaboral");
+        }
+        public IActionResult PersonalInformation()
+        {
+            return View("InformacionPersonal");
+        }
+    }
+}
---
+++ MovistarAPI/Controllers/SolutionController.cs
@@ -0,0 +1,33 @@
+﻿using Core;
+using Core.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovistarAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovistarAPI.Controllers
+{
+
+    [ApiController]
+    [Route("Solution")]
+    public class SolutionController : Controller
+    {
+        [HttpGet]
+        [Route("GetResultQuadraticFunction")]
+        public GenericResponse<QuadraticFunctionResult> GetResultQuadraticFunction(double valueA, double valueB, double valueC)
+        {
+            return RunFunction.Run<QuadraticFunctionResult>(() => Functions.GetResultQuadraticFunction(valueA, valueB, valueC));
+        }
+
+        [HttpGet]
+        [Route("GetResultFibonacci")]
+        public GenericResponse<List<long>> GetResultFibonacci(int limit)
+        {
+            return RunFunction.Run<List<long>>(() => Functions.GetResultFibonacci(limit));
+        }
+    }
+}
